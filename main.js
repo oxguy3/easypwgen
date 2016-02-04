62ff31075b759d424596617489ecaff57a47df77
Hayden Schiff <oxguy3@gmail.com>
2016-02-03 20:20:52 -0500

    cleanup, simplification, and documentation

--- main.js
+++ main.js
@@ -7,14 +7,17 @@ if (typeof wordlist === 'undefined') {
 
 } else {
 
+    // makes the first letter of a string uppercase, and the rest lowercase
     function capitalizeFirstLetter(text) {
         return text.charAt(0).toUpperCase() + text.substr(1).toLowerCase();
     }
 
     var goButton = document.getElementById("go");
 
-    // code to run when "Go!" button is pressed
+    // code to run when go button is pressed
     goButton.addEventListener("click", function(){
+
+        // get the word count from the input box
         var wordCount = parseInt(document.getElementById("wordCount").value, 10);
         if (isNaN(wordCount)) {
             alert("Invalid word count");
@@ -23,18 +26,20 @@ if (typeof wordlist === 'undefined') {
 
         var password = "";
 
+        // generate array of random numbers
         var randomNumbers = new Uint32Array(wordCount);
         window.crypto.getRandomValues(randomNumbers);
 
+        // use random number array to select words from the wordlist
         for (var i = 0; i < randomNumbers.length; i++) {
             var index = randomNumbers[i] % wordlist.length;
             password += capitalizeFirstLetter(wordlist[index]);
         }
 
+        // show the password to the user
         document.getElementById("output").innerHTML = password;
     });
 
     // now that all the code has run without crashing, make the Go! button clickable
     goButton.disabled = false;
-    document.getElementById("notLoaded").style.display = "none";
 }
